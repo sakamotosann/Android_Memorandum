afe6725df0fa624186cfb4f830653fc21f24b9f9
sakamotosann <15311937017@126.com>
2018-12-06 16:18:55 +0800

    2018-12-06 16:17:48

---
+++ app/src/main/java/com/pby/memo/AppendActivity.java
@@ -0,0 +1,17 @@
+package com.pby.memo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AppendActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_append);
+    }
+
+    public void onClick(View view) {
+    }
+}
--- app/src/main/java/com/pby/memo/MainActivity.java
+++ app/src/main/java/com/pby/memo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.pby.memo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button append;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        append = findViewById(R.id.append);
+
+        append.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.append) {
+            Intent intent = new Intent(MainActivity.this, AppendActivity.class);
+            startActivity(intent);
+        }
     }
 }
